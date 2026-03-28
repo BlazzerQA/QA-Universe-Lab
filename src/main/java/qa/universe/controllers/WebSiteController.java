554d3c207fb257a125294d0da6e7d5aa5ad7eb23
Dmitry Blazutskiy <dimab2000s@gmail.com>
2026-03-28 15:14:41 +0300

    Разработал страницу с авторизацией для будущих Ui-автотестов

--- src/main/java/qa/universe/controllers/WebSiteController.java
+++ src/main/java/qa/universe/controllers/WebSiteController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebSiteController {
 
-    @GetMapping("/welcome")
-    public String welcomePage(Model model) {
-        model.addAttribute("message","Привет, Spring работает!");
-        return "index";  //TODO: Разобраться почему не возвращается message
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/login"; // Перенаправляем пользователя
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage(Model model) {
+        model.addAttribute("message","QA Universe Auth");
+        return "login";  //TODO: Разобраться почему не возвращается message
     }
 }
