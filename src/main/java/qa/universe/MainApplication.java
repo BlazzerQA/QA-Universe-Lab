868768ade9f9491bcd537eaeedafea690c741038
Dmitry Blazutskiy <dimab2000s@gmail.com>
2026-04-02 23:47:15 +0300

    1)Настроена фильтрация aspectj (файлик aop) 2)Добавлены новые зависимости в pom.xml, которые связаны с SQLite 3)Переписана логика авторизации (Ui) 4)Создана база данных для работы с SQL запросами

--- src/main/java/qa/universe/MainApplication.java
+++ src/main/java/qa/universe/MainApplication.java
@@ -1,11 +1,25 @@
 package qa.universe;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import qa.universe.models.User;
+import qa.universe.repositories.UserRepository;
 
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initUsers(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.findUserByPhone("+79991234567").isEmpty()) {
+                userRepository.save(new User(null, "+79991234567", "password123"));
+                System.out.println("Тестовый пользователь создан!");
+            }
+        };
     }
 }
