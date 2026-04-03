52778458c1f457e57f05fcd77616fffd133ef9f2
Gleb Pivanov <okimaniiri@gmail.com>
2026-04-03 20:37:50 +0300

    Задание 2: Изменение контроллера products и добавление класса Product

---
+++ src/main/java/qa/universe/model/Product.java
@@ -0,0 +1,46 @@
+package qa.universe.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String productId;
+
+    @NotBlank(message = "Название продукта не может быть пустым")
+    private String productName;
+
+    @NotNull(message = "Цена не может быть пустой")
+    @Positive(message = "price должна быть положительна")  //может подойдет PositiveOrZero может ли цена быть 0?
+    private BigDecimal price;
+
+    public Product() {}
+    public Product(String productId, String productName, BigDecimal price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
+
