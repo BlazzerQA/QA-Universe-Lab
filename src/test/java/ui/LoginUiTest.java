4686e6a9b3e8d52ca1b74315aa0222e6ea5afd13
aigizdavletbaev111-ui <aigiz.davletbaev111@yandex.ru>
2026-04-22 01:22:31 +0300

    fix: обновлены локаторы и тестовые данные для LoginUiTest

--- src/test/java/ui/LoginUiTest.java
+++ src/test/java/ui/LoginUiTest.java
@@ -8,8 +8,7 @@ import pages.RoadmapPage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class LoginUiTest extends UiBaseTest {
 
@@ -19,47 +18,32 @@ public class LoginUiTest extends UiBaseTest {
     @Test
     @Description("Проверка полного успешного пути пользователя")
     public void testFullUserJourney() {
-
-        //1. Открываем страничку
         open("/login");
-
-        //2. Проверяем, что это она) по заголовку
         $("h1").shouldHave(text("QA Universe Auth"));
+        loginPage.login("+79991234567", "password123");
 
-        //3. Вводим успешные данные
-        loginPage.login("+79991112233", "qwerty");
+        // Просто ждём немного после логина
+        sleep(2000);
 
-        //5. Проверяем, что находимся на нужной странице
         roadmapPage.header.shouldHave(text("Roadmap: QA Automation"));
-
-        //6. Выходим по кнопке "Выйти"
         roadmapPage.logout();
-
-        //7. Проверяем, что вышли - просто если видим кнопку LOGIN
         $("button").shouldBe(visible);
     }
-
 
     @Test
     @Description("Проверка вывода ошибки при неверном пароле")
     public void testInvalidPasswordShowsError() {
         open("/login");
-
-        loginPage.login("+79991112233", "some-password");
-
+        loginPage.login("+79991234567", "wrong-password");
         loginPage.checkErrorMessage("Неверный пароль!");
-
         $("h1").shouldHave(text("QA Universe Auth"));
     }
-
 
     @Test
     @Description("Проверка вывода ошибки при вводе неверного формата номера")
     public void testInvalidPhoneFormat() {
         open("/login");
-
-        loginPage.login("89991112233", "qwerty");
-
+        loginPage.login("8999112233", "password123");
         loginPage.checkErrorMessage("Неверный формат номера. Используйте +7XXXXXXXXXX");
     }
 }
