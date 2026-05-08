9731b80f0a153a81b3ac6c2f0955fd849eebe4bf
DBlazutskiy <dimab2000s@gmail.com>
2026-05-08 21:30:08 +0300

    fix: [SECURITY] Доступ к магазину товаров (/products-ui) без авторизации не ограничен

--- src/main/java/qa/universe/controllers/ui/ProductsUIController.java
+++ src/main/java/qa/universe/controllers/ui/ProductsUIController.java
@@ -1,13 +1,17 @@
 package qa.universe.controllers.ui;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductsUIController {
-    
+
     @GetMapping("/products-ui")
-    public String productsUI() {
+    public String productsUI(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return "redirect:/login";
+        }
         return "products-ui";
     }
 }
